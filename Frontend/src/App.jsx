2f6e4e85b8858e5119cd8ae364075fbe0c2fbeb3
hashir689 <hashirhussain689@gmail.com>
2026-05-13 01:05:05 +0500

    Adding Frontend setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import { Button } from "./components/ui/button";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Navbar from "./components/navbar";
 
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    ),
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
 const App = () => {
   return (
     <>
-      <h1 className="text-center text-amber-500">Hello world</h1>
-      <Button>Click me</Button>
+      <RouterProvider router={router} />
     </>
   );
 };
