0945ea7329e9f0939c4f516b55369226eb658ccb
hashir689 <hashirhussain689@gmail.com>
2026-05-18 23:56:36 +0500

    Wrap up

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,15 @@ import express from "express";
 import "dotenv/config";
 import ConnectDb from "./Database/db.js";
 import userRoute from "./Routes/userRoute.route.js";
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT;
+app.use(
+  cors({
+    origin: "*",
+    Credential: true,
+  }),
+);
 app.use(express.json());
 app.use("/api/v1/user", userRoute);
 app.listen(PORT, () => {
