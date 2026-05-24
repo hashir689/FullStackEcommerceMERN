e70421ccd5ada8963be1aa46838de5032a0df185
hashir689 <hashirhussain689@gmail.com>
2026-05-24 19:39:35 +0500

    Adding and fixing bugs in Email verification

--- Frontend/src/pages/VerifyEmail.jsx
+++ Frontend/src/pages/VerifyEmail.jsx
@@ -1,9 +1,36 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const VerifyEmail = () => {
   const token = useParams();
   const [status, setstatus] = useState("Verifying...");
+  const navigate = useNavigate();
+  const verifyEmail = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/api/v1/user/verify",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      if (res.data.success) {
+        setstatus("Email has been verified");
+        setTimeout(() => {
+          navigate("/login");
+        }, 2000);
+      }
+    } catch (error) {
+      console.log(error);
+      setstatus("Verification has been Failed");
+    }
+  };
+  useEffect(() => {
+    verifyEmail();
+  }, [token]);
   return (
     <div className="relative w-full h-[760px] bg-pink-100 overflow-hidden">
       <div className="min-h-screen flex items-center justify-center">
